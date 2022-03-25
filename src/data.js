459b369e5582b41e987084ff0d28cc0366abc8db
Asensio0227 <mtheesikhosana@gmail.com>
2022-03-25 14:39:54 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+import hogan from './images/photo-2.jpg';
+import yates from './images/tree.jpg';
+import larry from './images/diamond1.jpg';
+import sean from './images/oliver.jpg';
+import lola from './images/mika-baumeister-Y_LgXwQEx2c-unsplash.jpg';
+
+const data = [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image: yates,
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image: hogan,
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image: larry,
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image: sean,
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image: lola,
+  },
+];
+
+export default data;
